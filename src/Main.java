4e4a6369e0e7aaf2a27c5af80cae69e170b719b6
Leva <parimalleva@gmail.com>
2021-06-19 13:01:51 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import basics01.Basics;
+
 public class Main {
     public static void main(String[] args) {
-      Basics
+        Basics.Solution();
     }
 }
--- src/basics01/Basics.java
+++ src/basics01/Basics.java
@@ -1,7 +1,13 @@
 package basics01;
 
+import basics01.tasks.Liner;
+
 public class Basics {
     public static void Solution() {
-        System.out.println("\t\t\t\t===|| 01 Basics ||===\n");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("\t\t\t\t===|| 01 Basics ||===");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println();
         Liner.Tasks();
+    }
 }
