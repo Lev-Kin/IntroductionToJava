b102a2e2a84c436a4f106615fc109965fd19a0c2
Leva <parimalleva@gmail.com>
2021-07-09 18:19:51 +0300

    #18 - add tasks in ArraysND

--- src/algorithm02/tasks/ArraysND.java
+++ src/algorithm02/tasks/ArraysND.java
@@ -1,14 +1,13 @@
 package algorithm02.tasks;
 
-import static supporting.MyTools.enterMatrixRandom;
-import static supporting.MyTools.printMatrix;
+import static supporting.MyTools.*;
 
 public class ArraysND {
     public static void Tasks() {
         System.out.println("\t\t\t=== Массивы массивов ===\n");
-        task01();
+        //task01();
         task02();
-//        task03();
+        task03();
 //        task04();
 //        task05();
 //        task06();
@@ -50,6 +49,72 @@ public class ArraysND {
                 "стоящие на диагонали.");
         System.out.println("-------------------------------------------------------------");
 
+        int m = 5;
+        int n = 5;
+
+        int[][] matrix = new int[m][n];
+
+        System.out.println("Дана матрица " + m + " X " + n);
+        enterMatrixRandom(matrix, m, n);
+        printMatrix(matrix, m, n);
+
+        System.out.println("Выводим все элементы, стоящие на диагонали.");
+        for (int i = 0; i < m; i++)
+            System.out.printf("[%d][%d] = %3d\n", i, i, matrix[i][i]);
+//        for (int i = 0; i < m; i++) {
+//            for (int j = n; j > -1; j--) {
+//                if (i == j){
+//                    System.out.printf("[%d][%d] = %3d ", i, j, matrix[i][j]);
+//                    break;
+//                }
+//            }
+//            System.out.println();
+//        }
+        System.out.println("=============================================================\n");
+    }
+
+    private static void task03() {
+        System.out.println("3. Дана матрица. Вывести k-ю строку и p-й столбец матрицы.");
+        System.out.println("-------------------------------------------------------------");
+
+        int m = 5;
+        int n = 5;
+        int k = 0;
+        int p = 0;
+
+        int[][] matrix = new int[m][n];
+
+        System.out.println("Дана матрица " + m + " X " + n);
+        enterMatrixRandom(matrix, m, n);
+        printMatrix(matrix, m, n);
+
+        do {
+            System.out.println("\nНеобходимо ввести число k строку матрицы");
+            k = scannerInt();
+            if ((k + 1) > m || k < 0)
+                System.out.println("Число k выходит за приделы матрицы! от 0 до " + (m - 1));
+        } while ((k + 1) > m || k < 0);
+        System.out.println(".   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .");
+
+        do {
+            System.out.println("\nНеобходимо ввести число p столбца матрицы");
+            p = scannerInt();
+            if ((p + 1) > n || p < 0)
+                System.out.println("Число p выходит за приделы матрицы! oт 0 до " + (n - 1));
+        } while ((p + 1) > n || p < 0);
+
+        System.out.println(".   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .");
+        System.out.println("Выводим k = " + k + " строку матрицы:");
+        for (int i = 0; i < m; i++) {
+            System.out.printf("[%d][%d] = %3d ", k, i, matrix[k][i]);
+        }
+        System.out.println();
+        System.out.println(".   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .");
+
+        System.out.println("Выводим p = " + p + " столбец матрицы:");
+        for (int j = 0; j < n; j++) {
+            System.out.printf("[%d][%d] = %3d\n", j, p, matrix[j][p]);
+        }
         System.out.println("=============================================================\n");
     }
 
