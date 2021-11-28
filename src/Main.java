1274364e4c6527fc2121ef4a9e20d036ff4b51a9
Leva <parimalleva@gmail.com>
2021-11-28 11:50:15 +0300

    #64 - minor adjustments

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
+import u01basics.Basics;
+import u02algorithm.Algorithmization;
+import u03strings.Strings;
 import u04сlasses.Classes;
 import u05oop.BasicsOfOOP;
 import u06finish.Finish;
 
 public class Main {
     public static void main(String[] args) {
-        //Basics.Solution();
+        Basics.Solution();
         //Algorithmization.Solution();
         //Strings.Solution();
         //Classes.Solution();
         //BasicsOfOOP.Solution();
-        Finish.Solution();
+        //Finish.Solution();
     }
 }
